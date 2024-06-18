40f1965147b47408a4f15ffe92b5a53be7dcb3d5
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-06-18 09:32:26 +0200

    Add test to wiremock initializer

--- src/test/java/com/eightbits/http/wiremock/WireMockInitializerTest.java
+++ src/test/java/com/eightbits/http/wiremock/WireMockInitializerTest.java
@@ -3,7 +3,16 @@ package com.eightbits.http.wiremock;
 import com.eightbits.http.wiremock.initializer.WireMockContextInitializer;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +20,9 @@ class WireMockInitializerTest {
 
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner();
 
+
     @Test
-    void custom_context_initializer() {
+    void ensure_wiremock_is_running() {
         this.contextRunner
                 .withInitializer(new WireMockContextInitializer())
                 .run(context -> {
@@ -22,5 +32,40 @@ class WireMockInitializerTest {
                     WireMockServer wiremock = context.getBean(WireMockServer.class);
                     assertThat(wiremock.isRunning()).isTrue();
                 });
+    }
+
+    @Test
+    void wiremock_endpoint_should_be_represented_in_the_properties() {
+        this.contextRunner
+                .withInitializer(new WireMockContextInitializer())
+                .withInitializer(this::loadTestProperties).run(context -> {
+                    assertThat(context.getEnvironment().getProperty("http.async.webclient.base-url")).isNotNull();
+                    assertThat(context.getEnvironment().getProperty("http.async.webclient.base-url")).matches("http://localhost:\\d+");
+                });
+    }
+
+    private void loadTestProperties(ConfigurableApplicationContext applicationContext) {
+        try {
+            GenericApplicationContext genericContext = (GenericApplicationContext) applicationContext;
+            genericContext.getEnvironment().getPropertySources()
+                    .addFirst(new ResourcePropertySource("classpath:application-test.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load properties from application-test.properties", e);
+        }
+    }
+
+    private void printProperties(AssertableApplicationContext context) {
+        // Get the environment
+        ConfigurableEnvironment environment = context.getEnvironment();
+
+        // Iterate through property sources and print properties
+        for (PropertySource<?> propertySource : environment.getPropertySources()) {
+            if (propertySource.getSource() instanceof Map) {
+                Map<String, Object> source = (Map<String, Object>) propertySource.getSource();
+                for (Map.Entry<String, Object> entry : source.entrySet()) {
+                    System.out.println(entry.getKey() + "=" + entry.getValue());
+                }
+            }
+        }
     }
 }
